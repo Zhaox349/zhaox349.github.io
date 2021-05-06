f7ce74d8c6f1fb0ddc82ce65eb37a3813ef296c3
Chiiiz <zhaox349@newschool.edu>
2021-05-06 12:59:15 +0800

    change css style and position

---
+++ Input:Output/map.js
@@ -0,0 +1,88 @@
+
+console.log('find me in main.js');
+
+//生成从minNum到maxNum的随机数
+function randomNum(minNum,maxNum){ 
+  return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+} 
+var container = document.querySelector('.container');
+
+var place = document.createElement('div');
+place.classList.add("container");
+document.querySelector(".container").append(place);
+
+var initImages=[
+  './fruit/L.png',
+  './fruit/E.png',
+  './fruit/T.png',
+  './fruit/S.png',
+  './fruit/plate.png',
+  './fruit/L.png',
+  './fruit/E.png',
+  './fruit/A.png',
+  './fruit/R.png',
+  './fruit/N.png'
+]
+var allFruits=[
+  'apple',
+  'banana',
+  'grape',
+  'lemon',
+  'lychee',
+  'mango',
+  'orange',
+  'peach',
+  'pear',
+  'pineapple',
+  'strawberry',
+  'watermelon',
+]
+var currentFruit;
+for(var i=0;i<initImages.length;i++){
+  var photoUrl = document.createElement('img');
+  photoUrl.classList.add('item');
+  photoUrl.src = initImages[i];
+  
+  photoUrl.addEventListener('mouseover',function(){
+    let index=randomNum(0,11);
+    this.src='./fruit/'+allFruits[index]+'.png';
+    currentFruit=allFruits[index];
+  })
+  photoUrl.addEventListener('click',function(){
+    var mp3 = "./sound/"+currentFruit+".mp3";
+    var mp3 = new Audio(mp3);
+    mp3.play();
+  })
+
+  container.append(photoUrl);
+}
+
+for(var i=0;i<78;i++){
+  var photoUrl = document.createElement('img');
+  photoUrl.classList.add('item');
+  photoUrl.src = './fruit/plate.png';
+
+  photoUrl.addEventListener('mouseover',function(){
+    let index=randomNum(0,11);
+    this.src='./fruit/'+allFruits[index]+'.png';
+    currentFruit=allFruits[index];
+  })
+  photoUrl.addEventListener('click',function(){
+    var mp3 = "./sound/"+currentFruit+".mp3";
+    var mp3 = new Audio(mp3);
+    mp3.play();
+  })
+
+  container.append(photoUrl);
+}
+
+var nextId = document.querySelector('#nextId');
+nextId.addEventListener('click',function(){
+  location.href="./word.html"
+})
+
+var player = document.querySelector('#myAudio');
+document.addEventListener('click',function(){
+  player.play();
+  player.volume = 0.35;
+})
---
+++ Input:Output/word.js
@@ -0,0 +1,135 @@
+var allFruits=[
+  'apple',
+  'banana',
+  'grape',
+  'lemon',
+  'lychee',
+  'mango',
+  'orange',
+  'peach',
+  'pear',
+  'pineapple',
+  'strawberry',
+  'watermelon',
+]
+var upId = document.querySelector('#upId');
+var downId = document.querySelector('#downId');
+var firstId = document.querySelector('#firstId');
+var secondId = document.querySelector('#secondId');
+var thirdId = document.querySelector('#thirdId');
+var fruitId = document.querySelector('#fruitId');
+var nameId = document.querySelector('#nameId');
+var soundId = document.querySelector('#soundId');
+var submitId = document.querySelector('#submitId');
+var inputId = document.querySelector('#inputId');
+var reviewId = document.querySelector('#reviewId');
+
+
+
+function getSelectImg(selectImgIndex){
+  console.log('selectImgIndex111111111111');
+  console.log(selectImgIndex);
+  firstId.innerHTML='';
+  var firstImg = document.createElement('img');
+  firstImg.classList.add('selectImg');
+  firstImg.src='./fruit/'+allFruits[selectImgIndex]+'.png';
+  firstId.append(firstImg);
+  
+  secondId.innerHTML='';
+  var secondImg = document.createElement('img');
+  secondImg.classList.add('selectImg');
+  secondImg.src='./fruit/'+allFruits[selectImgIndex+1]+'.png';
+  secondId.append(secondImg);
+  
+  thirdId.innerHTML='';
+  var thirdImg = document.createElement('img');
+  thirdImg.classList.add('selectImg');
+  thirdImg.src='./fruit/'+allFruits[selectImgIndex+2]+'.png';
+  thirdId.append(thirdImg);
+}
+
+var selectImgIndex=0;
+let currentFruit='apple';
+getSelectImg(selectImgIndex);
+fruitId.innerHTML='';
+var fruitImg = document.createElement('img');
+fruitImg.classList.add('fruitImg');
+fruitImg.src='./fruit/'+currentFruit+'.png';
+fruitId.append(fruitImg);
+nameId.innerHTML=currentFruit;
+
+
+
+upId.addEventListener('click',function(){
+  if(selectImgIndex>=allFruits.length-3){
+    return;
+  }
+  selectImgIndex=selectImgIndex+3;
+  getSelectImg(selectImgIndex);
+})
+downId.addEventListener('click',function(){
+  if(selectImgIndex<=0){
+    return;
+  }
+  selectImgIndex=selectImgIndex-3;
+  getSelectImg(selectImgIndex);
+})
+
+
+firstId.addEventListener('click',function(){
+  fruitId.innerHTML='';
+  var fruitImg = document.createElement('img');
+  fruitImg.classList.add('fruitImg');
+  fruitImg.src='./fruit/'+allFruits[selectImgIndex]+'.png';
+  fruitId.append(fruitImg);
+  nameId.innerHTML=allFruits[selectImgIndex];
+  currentFruit=allFruits[selectImgIndex];
+   inputId.value = "";
+})
+secondId.addEventListener('click',function(){
+  fruitId.innerHTML='';
+  var fruitImg = document.createElement('img');
+  fruitImg.classList.add('fruitImg');
+  fruitImg.src='./fruit/'+allFruits[selectImgIndex+1]+'.png';
+  fruitId.append(fruitImg);
+  nameId.innerHTML=allFruits[selectImgIndex+1];
+  currentFruit=allFruits[selectImgIndex+1];
+   inputId.value = "";
+})
+thirdId.addEventListener('click',function(){
+  fruitId.innerHTML='';
+  var fruitImg = document.createElement('img');
+  fruitImg.classList.add('fruitImg');
+  fruitImg.src='./fruit/'+allFruits[selectImgIndex+2]+'.png';
+  fruitId.append(fruitImg);
+  nameId.innerHTML=allFruits[selectImgIndex+2];
+  currentFruit=allFruits[selectImgIndex+2];
+      inputId.value = "";
+})
+
+soundId.addEventListener('click',function(){
+  var mp3 = "./sound/"+currentFruit+".mp3";
+  var mp3 = new Audio(mp3);
+  mp3.play();
+})
+
+submitId.addEventListener('click',function(){
+  if(inputId.value!==undefined&&inputId.value!==null&&inputId.value.toString().trim()==currentFruit){
+    var mp3 = "./sound/"+currentFruit+".mp3";
+    var mp3 = new Audio(mp3);
+    mp3.play();
+    // inputId.value = "";
+  }
+
+})
+
+reviewId.addEventListener('click',function(){
+  location.href="./map.html"
+})
+
+
+var player = document.querySelector('#myAudio');
+document.addEventListener('click',function(){
+  player.play();
+  player.volume = 0.35;
+})
